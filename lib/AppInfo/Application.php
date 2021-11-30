d09cc7d509ac733f604ee87213f4afd7fb3070ba
Muzosh <30979983+Muzosh@users.noreply.github.com>
2021-11-30 17:04:02 +0000

    format all

--- lib/AppInfo/Application.php
+++ lib/AppInfo/Application.php
@@ -6,10 +6,12 @@ namespace OCA\TwoFactorSmartCard\AppInfo;
 
 use OCP\AppFramework\App;
 
-class Application extends App {
+class Application extends App
+{
 	public const APP_NAME = 'twofactor_smartcard';
 
-	public function __construct(array $urlParams = []) {
+	public function __construct(array $urlParams = [])
+	{
 		parent::__construct(self::APP_NAME, $urlParams);
 	}
 }
