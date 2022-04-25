4bb4560c37122f67f643f0eb46bd5a3f11ee8f3c
Muzosh <muzosak@seznam.cz>
2022-04-25 11:06:51 +0000

    Change message

--- templates/challenge.php
+++ templates/challenge.php
@@ -4,7 +4,7 @@ style('twofactor_smartcard', 'style');
 
 <img class="two-factor-icon" src="<?php print_unescaped(image_path('twofactor_smartcard', 'app.svg')); ?>" alt="">
 
-<p><?php p('Connector must be running on your machine!') ?></p>
+<p><?php p('Custom Web-eID solution must be installed on your machine!') ?></p>
 <p><?php p('Please insert your smartcard and click on button below.') ?></p>
 
 <form name="smartcardform" method="POST" class="smartcard-form">
