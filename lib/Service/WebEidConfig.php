a63cf10a2bc86a0fe2b536e78e71d70350c7f55d
Muzosh <muzosak@seznam.cz>
2022-05-28 20:06:15 +0000

    hotfix: create config file and use main branch of authtoken validation library

---
+++ lib/Service/WebEidConfig.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ *
+ * @copyright Copyright (c) 2022, Petr Muzikant (petr.muzikant@vut.cz)
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+declare(strict_types=1);
+
+namespace OCA\TwoFactorWebEid\Service;
+
+use ArrayAccess;
+
+class WebEidConfig implements ArrayAccess {
+	private array $configArray;
+
+	public function __construct() {
+		$this->configArray = array(
+			'CHALLENGE_NONCE_TTL_SECONDS' => 300,
+			'TRUSTED_CERT_PATH' => __DIR__.'/../../trustedcerts',
+			'ORIGIN' => 'https://'.$_SERVER['SERVER_ADDR']
+		);
+	}
+
+	public function offsetExists($offset): bool {
+		return isset($this->configArray[$offset]);
+	}
+
+	public function offsetGet($offset) {
+		return $this->configArray[$offset];
+	}
+
+	public function offsetUnset($offset): void {
+		unset($this->configArray[$offset]);
+	}
+
+	public function offsetSet($offset, $value): void {
+		$this->configArray[$offset] = $value;
+	}
+}
